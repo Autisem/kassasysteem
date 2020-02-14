b384779508f5eedfa012266efef314c25659c4dc
BotSquirrel <lukaleppens@gmail.com>
2020-02-14 12:58:45 +0100

    index header and footer php fix

---
+++ header.php
@@ -0,0 +1,36 @@
+<!doctype html>
+<html class="no-js" lang="EN">
+
+<head>
+  <meta charset="utf-8">
+  <title>Bioscoop AMO</title>
+  <meta name="description" content="">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+
+  <link rel="manifest" href="site.webmanifest">
+  <link rel="apple-touch-icon" href="icon.png">
+  <link href="https://fonts.googleapis.com/css?family=Oswald|PT+Sans&display=swap" rel="stylesheet">
+  <!-- Place favicon.ico in the root directory -->
+
+  <link rel="stylesheet" href="css/normalize.css">
+  <link rel="stylesheet" href="css/main.css">
+
+  <meta name="theme-color" content="#fafafa">
+</head>
+
+<body>
+
+  <header class="flex">
+    <div class="brand">
+        <h1>Bioscoop AMO</h1>
+    </div>
+    <nav>
+      <a href="">Films</a>
+      <a href="">Nu</a>
+      <a href="">Binnenkort</a>
+    </nav>
+    <form action="">
+      <a href="#">Account</a>
+      <input type="text" placeholder="search">
+    </form>
+  </header>
