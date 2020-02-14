8e1577368a07c0380b6b7e75fa05ed4c18ba6a2a
Martijn <martijnfox@outlook.com>
2020-02-14 15:04:32 +0100

    php aangevuld lig in systeem

--- login_systeem/aanmelden/aanmelden.php
+++ login_systeem/aanmelden/aanmelden.php
@@ -1,3 +1,15 @@
 <?php
+$voornaam = $_POST["voornaam-input"];
+$achternaam = $_POST["achternaam-input"];
+$geboortedatum = $_POST["geboortedatum-input"];
+$woonplaats = $_POST["woonplaats-input"];
+$straatnaam = $_POST["straatnaam-input"];
+$huisnummer = $_POST["huisnummer-input"];
+$postcode = $_POST["postcode-input"];
+$tefoonnummer = $_POST["telefoonnummer-input"];
+$email = $_POST["email-input"];
+$emailh= $_POST["email-two-input"];
+$wachtwoord = $_POST["wachtwoord-input"];
+$wachtwoordh = $_POST["wachtwoord-two-input"];
 
  ?>
