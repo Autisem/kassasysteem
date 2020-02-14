22c680bd8d588cce6165e5562858f0e4ae91d635
Autisem <36102060+Autisem@users.noreply.github.com>
2020-02-14 13:23:36 +0100

    GetBar now has return type, echo to display it

--- php/BarCodeGen.php
+++ php/BarCodeGen.php
@@ -6,12 +6,12 @@ echo '<form method="post" action=""><input type="text" name="code">
       <input type="submit">
       </form>';
 
-GetBar($_POST['code']);
+echo GetBar($_POST['code']);
 
 function GetBar($code){
 $generator = new Picqer\Barcode\BarcodeGeneratorHTML();
 if(isset($code)){
-echo $generator->getBarcode($code, $generator::TYPE_CODE_128);
+return $generator->getBarcode($code, $generator::TYPE_CODE_128);
 }
 }
 
