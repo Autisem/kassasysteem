4655c31ecaf3d18202f1bd962a9bccb2c8e95ad7
Autisem <noah_wieberdink@hotmail.com>
2020-02-17 15:36:51 +0100

    Orders can now be processed my form

--- order.php
+++ order.php
@@ -2,9 +2,17 @@
 
 require 'header.php';
 require 'php/dbsuit.php';
-$datetime = date_create()->format('Y-m-d H:i:s');
+echo '<form action="" method="post">Movie name:<input type="text" name="name">
+      User ID:<input type="text" name="user">
+      Date: <input type="date" name="time">
+      <input type="submit">
+      </form>';
+if(!isset($_POST)){
+$datestro = strtotime($_POST['time']);
+$datetime = date('Y-m-d H:i:s', $datestro);
 
-CreateOrder($db, 1,"Spiderverse", $datetime);
+CreateOrder($db, $_POST['user'],$_POST['name'], $datetime);
 echo "made order!";
+}
 require 'footer.php';
 ?>
