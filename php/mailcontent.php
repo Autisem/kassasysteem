9c5ef1354f87829a9607320c9d92c55cde393fc2
Daanisaanwezig <Daanisaanwezig@outlook.com>
2020-02-19 14:52:08 +0100

    library update

---
+++ php/mailcontent.php
@@ -0,0 +1,19 @@
+<?php
+function header($title)
+{
+  $mailContent =
+  "
+  <html>
+    <head>
+    </head>
+
+    <body>
+      <header style="background-color: \"#0c0c0c;\">
+
+      </header>
+    </body>
+  </html>
+  ";
+}
+
+?>
