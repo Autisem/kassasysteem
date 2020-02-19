595cd4984206a0a42ce9fcda9b93b30bb7e57d23
Daanisaanwezig <Daanisaanwezig@outlook.com>
2020-02-19 13:41:17 +0100

    Update for the sendMail function

--- php/sendmail.php
+++ php/sendmail.php
@@ -11,8 +11,15 @@ require 'vendor\autoload.php';
 /*
 *
 * How the function works:
-* The function will send the user an email with html support, the mail
-*
+* The function will send the user an email with html support, the mail will be send from daanisaanwezigdev@gmail.com for now, this can be changed later
+
+* The variables:
+* $mailAdress - you should put the mail adress of the person you want to send the mail to here
+* $userName - this should be the name of the person the mail will be send to
+* $subject - this is the title of the mail
+* $mailBody - This should be the content of the mail, you can use html here if you wish to
+* $altMailBody - this will be used in case the normal mail body can not be loaded, this should be in a plain text format
+* $attachment - this is optional and can be left empty, this will add a file or image to the mail as an attachment
 */
 function SendMail($mailAdress, $userName, $subject, $mailBody, $altMailBody, $attachment)
 {
