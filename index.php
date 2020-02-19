62d7c3848b96ea67c0483d4bf8ede6950fe9d6a9
Gnol Gnart <longtrang@live.nl>
2020-02-19 14:46:58 +0100

    add nu, binnenkort and nu section + title shadow

--- index.php
+++ index.php
@@ -6,39 +6,11 @@
       <h2 class="herotitle">
         Populair
       </h2>
-      <!-- 
-      <a href="#"><img src="https://media.pathe.nl/thumb/360x508/gfx_content/other/api/filmdepot/v1/movie/download/33234_121015_ps_sd-high.jpg"></a>
-      <a href="#"><img src="https://media.pathe.nl/sticker/kids/gfx_content/other/api/filmdepot/v1/movie/download/32957_119635_ps_sd-high.jpg"></a>
-      <a href="#"><img src="https://media.pathe.nl/sticker/pac/gfx_content/other/api/filmdepot/v1/movie/download/32970_117054_ps_sd-high.jpg"></a>
-      <a href="#"><img src="https://media.pathe.nl/thumb/360x508/gfx_content/posterhr/Sonic-OV-_ps_2_jpg_sd-high_Copyright-2019-Paramount-Pictures-and-Sega-of-America-Inc-All-Rights-Reserved.jpg"></a>
-      <a href="#"><img src="https://media.pathe.nl/thumb/360x508/gfx_content/other/api/filmdepot/v1/movie/download/32699_122036_ps_sd-high.jpg"></a>
-      <a href="#"><img src="https://media.pathe.nl/thumb/360x508/gfx_content/posterhr/OJIM_POSTER_70x100cm_3000px.jpg"></a>
-      <a href="#"><img src="https://media.pathe.nl/thumb/360x508/gfx_content/other/api/filmdepot/v1/movie/download/27096_118810_ps_sd-high.jpg"></a>
-      <a href="#"><img src="https://media.pathe.nl/thumb/360x508/gfx_content/posterhr/Little-Women_ps_1_jpg_sd-high_Copyright-2019-CTMG-Inc-All-Rights-Reserved.jpg"></a>
-      <a href="#"><img src="https://media.pathe.nl/thumb/360x508/gfx_content/posterhr/Kruimeltje_ps_1_jpg_sd-high_Copyright-2019-WW-Entertainment.jpg"></a>
-      <a href="#"><img src="https://media.pathe.nl/thumb/360x508/gfx_content/posterhr/Dolittle-Pathe-Posterv2.jpg"></a>
-      <a href="#"><img src="https://media.pathe.nl/thumb/360x508/gfx_content/other/api/filmdepot/v1/movie/download/32751_118037_ps_sd-high.jpg"></a>
-      <a href="#"><img src="https://media.pathe.nl/thumb/360x508/gfx_content/posterhr/Joker_ps_1_jpg_sd-high_-2019-Warner-Bros-Entertainment-Inc-All-Rights-Reserved-Photo-Credit-Niko-Tavernise.jpg"></a>
-      <a href="#"><img src="https://media.pathe.nl/thumb/360x508/gfx_content/posterhr/Frozen-2-NL-_ps_1_jpg_sd-high_Copyright-2019-Disney-Enterprises-Inc-All-Rights-Reserved.jpg"></a>
-      <a href="#"><img src="https://media.pathe.nl/thumb/360x508/gfx_content/posterhr/Jumanji_-The-Next-Level_ps_1_jpg_sd-high_Copyright-2019-CTMG-Inc-All-Rights-Reserved.jpg"></a>
-      <a href="#"><img src="https://media.pathe.nl/thumb/360x508/gfx_content/other/api/filmdepot/v1/movie/download/32902_116075_ps_sd-high.jpg"></a>
-      <a href="#"><img src="https://media.pathe.nl/thumb/360x508/gfx_content/posters/starwarsriseofskywalkerlowres.png"></a>
-      <a href="#"><img src="https://media.pathe.nl/thumb/360x508/gfx_content/other/api/filmdepot/v1/movie/download/28438_114828_ps_sd-high.jpg"></a> 
-      -->
       <div class="text-center">
         <div class="carousel slide row" data-ride="carousel" data-type="multi" id="moviescarousel">
 
           <div class="carousel-inner">
             <div class="item active">
-              <div class="col-md-3 col-sm-4 col-xs-12"><a href="#"><img src="https://media.pathe.nl/thumb/360x508/gfx_content/posterhr/Birds-of-Prey-And-The-Fantabulous-Emancipation-Of-One-Harley-Quinn-_ps_1_jpg_sd-high_Copyright-2019-WARNER-BROS-ENTERTAINMENT-INC-Photo-Credit-JAKE-GILES-NETTER.jpg" class="img-responsive"></a></div>
-            </div>
-            <div class="item">
-              <div class="col-md-3 col-sm-4 col-xs-12"><a href="#"><img src="https://media.pathe.nl/thumb/360x508/gfx_content/other/api/filmdepot/v1/movie/download/33300_120805_ps_sd-high.jpg" class="img-responsive"></a></div>
-            </div>
-            <div class="item">
-              <div class="col-md-3 col-sm-4 col-xs-12"><a href="#"><img src="https://media.pathe.nl/thumb/360x508/gfx_content/posterhr/Bad-Boys-For-Life_ps_2_jpg_sd-high_Copyright-2019-CTMG-Inc-All-Rights-Reserved.jpg" class="img-responsive"></a></div>
-            </div>
-            <div class="item">
               <div class="col-md-3 col-sm-4 col-xs-12"><a href="#"><img src="https://media.pathe.nl/thumb/360x508/gfx_content/other/api/filmdepot/v1/movie/download/32832_117475_ps_sd-high.jpg" class="img-responsive"></a></div>
             </div>
             <div class="item">
@@ -46,6 +18,21 @@
             </div>
             <div class="item">
               <div class="col-md-3 col-sm-4 col-xs-12"><a href="#"><img src="https://media.pathe.nl/thumb/360x508/gfx_content/posters/No-Time-to-Die_ps_1_jpg_sd-low.jpg" class="img-responsive"></a></div>
+            </div>
+            <div class="item">
+              <div class="col-md-3 col-sm-4 col-xs-12"><a href="#"><img src="https://media.pathe.nl/thumb/360x508/gfx_content/other/api/filmdepot/v1/movie/download/32751_118037_ps_sd-high.jpg" class="img-responsive"></a></div>
+            </div>
+            <div class="item">
+              <div class="col-md-3 col-sm-4 col-xs-12"><a href="#"><img src="https://media.pathe.nl/thumb/360x508/gfx_content/posterhr/Birds-of-Prey-And-The-Fantabulous-Emancipation-Of-One-Harley-Quinn-_ps_1_jpg_sd-high_Copyright-2019-WARNER-BROS-ENTERTAINMENT-INC-Photo-Credit-JAKE-GILES-NETTER.jpg" class="img-responsive"></a></div>
+            </div>
+            <div class="item">
+              <div class="col-md-3 col-sm-4 col-xs-12"><a href="#"><img src="https://media.pathe.nl/thumb/360x508/gfx_content/other/api/filmdepot/v1/movie/download/32699_122036_ps_sd-high.jpg" class="img-responsive"></a></div>
+            </div>
+            <div class="item">
+              <div class="col-md-3 col-sm-4 col-xs-12"><a href="#"><img src="https://media.pathe.nl/thumb/360x508/gfx_content/posterhr/Bad-Boys-For-Life_ps_2_jpg_sd-high_Copyright-2019-CTMG-Inc-All-Rights-Reserved.jpg" class="img-responsive"></a></div>
+            </div>
+            <div class="item">
+              <div class="col-md-3 col-sm-4 col-xs-12"><a href="#"><img src="https://media.pathe.nl/thumb/360x508/gfx_content/posterhr/Joker_ps_1_jpg_sd-high_-2019-Warner-Bros-Entertainment-Inc-All-Rights-Reserved-Photo-Credit-Niko-Tavernise.jpg" class="img-responsive"></a></div>
             </div>
           </div>
 
@@ -57,10 +44,48 @@
   </section>
   <section id="nu" class="sectionspacing">
     <div class="wrapper">
-      <div class="nugrid">
+      <h2 class="subtitle">
+        Nu
+      </h2>
+      <div class="sectiongrid">
         <div>
           <a href="">
-            <img src="https://media.pathe.nl/thumb/360x508/gfx_content/other/api/filmdepot/v1/movie/download/33234_121015_ps_sd-high.jpg" alt="movie">
+            <img src="https://media.pathe.nl/thumb/360x508/gfx_content/other/api/filmdepot/v1/movie/download/32832_117475_ps_sd-high.jpg" alt="movie">
+          </a>
+        </div>
+        <div>
+          <a href="">
+            <img src="https://media.pathe.nl/thumb/360x508/gfx_content/posterhr/1917_ps_1_jpg_sd-high_Copyright-2019-WW-Entertainment-2.jpeg" alt="movie">
+          </a>
+        </div>
+        <div>
+          <a href="">
+            <img src="https://media.pathe.nl/thumb/360x508/gfx_content/posters/No-Time-to-Die_ps_1_jpg_sd-low.jpg" alt="movie">
+          </a>
+        </div>
+        <div>
+          <a href="">
+            <img src="https://media.pathe.nl/thumb/360x508/gfx_content/other/api/filmdepot/v1/movie/download/32751_118037_ps_sd-high.jpg" alt="movie">
+          </a>
+        </div>
+        <div>
+          <a href="">
+            <img src="https://media.pathe.nl/thumb/360x508/gfx_content/posterhr/Birds-of-Prey-And-The-Fantabulous-Emancipation-Of-One-Harley-Quinn-_ps_1_jpg_sd-high_Copyright-2019-WARNER-BROS-ENTERTAINMENT-INC-Photo-Credit-JAKE-GILES-NETTER.jpg" alt="movie">
+          </a>
+        </div>
+        <div>
+          <a href="">
+            <img src="https://media.pathe.nl/thumb/360x508/gfx_content/other/api/filmdepot/v1/movie/download/32699_122036_ps_sd-high.jpg" alt="movie">
+          </a>
+        </div>
+        <div>
+          <a href="">
+            <img src="https://media.pathe.nl/thumb/360x508/gfx_content/posterhr/Bad-Boys-For-Life_ps_2_jpg_sd-high_Copyright-2019-CTMG-Inc-All-Rights-Reserved.jpg" alt="movie">
+          </a>
+        </div>
+        <div>
+          <a href="">
+            <img src="https://media.pathe.nl/thumb/360x508/gfx_content/posterhr/Joker_ps_1_jpg_sd-high_-2019-Warner-Bros-Entertainment-Inc-All-Rights-Reserved-Photo-Credit-Niko-Tavernise.jpg" alt="movie">
           </a>
         </div>
         <div>
@@ -70,53 +95,78 @@
         </div>
         <div>
           <a href="">
-            <img src="https://media.pathe.nl/thumb/360x508/gfx_content/other/api/filmdepot/v1/movie/download/33234_121015_ps_sd-high.jpg" alt="movie">
-          </a>
-        </div>
-        <div>
-          <a href="">
-            <img src="https://media.pathe.nl/thumb/360x508/gfx_content/other/api/filmdepot/v1/movie/download/33234_121015_ps_sd-high.jpg" alt="movie">
-          </a>
-        </div>
-        <div>
-          <a href="">
-            <img src="https://media.pathe.nl/thumb/360x508/gfx_content/other/api/filmdepot/v1/movie/download/33234_121015_ps_sd-high.jpg" alt="movie">
-          </a>
-        </div>
-        <div>
-          <a href="">
-            <img src="https://media.pathe.nl/thumb/360x508/gfx_content/other/api/filmdepot/v1/movie/download/33234_121015_ps_sd-high.jpg" alt="movie">
-          </a>
-        </div>
-        <div>
-          <a href="">
-            <img src="https://media.pathe.nl/thumb/360x508/gfx_content/other/api/filmdepot/v1/movie/download/33234_121015_ps_sd-high.jpg" alt="movie">
-          </a>
-        </div>
-        <div>
-          <a href="">
-            <img src="https://media.pathe.nl/thumb/360x508/gfx_content/other/api/filmdepot/v1/movie/download/33234_121015_ps_sd-high.jpg" alt="movie">
-          </a>
-        </div>
-        <div>
-          <a href="">
-            <img src="https://media.pathe.nl/thumb/360x508/gfx_content/other/api/filmdepot/v1/movie/download/33234_121015_ps_sd-high.jpg" alt="movie">
-          </a>
-        </div>
-        <div>
-          <a href="">
-            <img src="https://media.pathe.nl/thumb/360x508/gfx_content/other/api/filmdepot/v1/movie/download/33234_121015_ps_sd-high.jpg" alt="movie">
+            <img src="https://media.pathe.nl/thumb/360x508/gfx_content/posterhr/Frozen-2-NL-_ps_1_jpg_sd-high_Copyright-2019-Disney-Enterprises-Inc-All-Rights-Reserved.jpg" alt="movie">
           </a>
         </div>
       </div>
     </div>
   </section>
-  <section id="Binnenkort" class="sectionspacing">
+  <section id="binnenkort" class="sectionspacing">
     <div class="wrapper">
+      <h2 class="subtitle">
+        Binnenkort
+      </h2>
+      <div class="sectiongrid">
+        <div>
+          <a href="">
+            <img src="https://media.pathe.nl/thumb/360x508/gfx_content/posterhr/Sonic-OV-_ps_2_jpg_sd-high_Copyright-2019-Paramount-Pictures-and-Sega-of-America-Inc-All-Rights-Reserved.jpg" alt="movie">
+          </a>
+        </div>
+        <div>
+          <a href="">
+            <img src="https://media.pathe.nl/thumb/360x508/gfx_content/other/api/filmdepot/v1/movie/download/27096_118810_ps_sd-high.jpg" alt="movie">
+          </a>
+        </div>
+        <div>
+          <a href="">
+            <img src="https://media.pathe.nl/thumb/360x508/gfx_content/posters/starwarsriseofskywalkerlowres.png" alt="movie">
+          </a>
+        </div>
+        <div>
+          <a href="">
+            <img src="https://media.pathe.nl/thumb/360x508/gfx_content/other/api/filmdepot/v1/movie/download/32902_116075_ps_sd-high.jpg" alt="movie">
+          </a>
+        </div>
+        <div>
+          <a href="">
+            <img src="https://media.pathe.nl/thumb/360x508/gfx_content/posterhr/Dolittle-Pathe-Posterv2.jpg" alt="movie">
+          </a>
+        </div>
+      </div>
     </div>
   </section>
-  <section id="Kids" class="sectionspacing">
+  <section id="kids" class="sectionspacing">
     <div class="wrapper">
+      <h2 class="subtitle">
+        Kids
+      </h2>
+      <div class="sectiongrid">
+        <div>
+          <a href="">
+            <img src="https://media.pathe.nl/thumb/360x508/gfx_content/other/api/filmdepot/v1/movie/download/33300_120805_ps_sd-high.jpg" alt="movie">
+          </a>
+        </div>
+        <div>
+          <a href="">
+            <img src="https://media.pathe.nl/sticker/kids/gfx_content/other/api/filmdepot/v1/movie/download/32957_119635_ps_sd-high.jpg" alt="movie">
+          </a>
+        </div>
+        <div>
+          <a href="">
+            <img src="https://media.pathe.nl/thumb/360x508/gfx_content/other/api/filmdepot/v1/movie/download/28438_114828_ps_sd-high.jpg" alt="movie">
+          </a>
+        </div>
+        <div>
+          <a href="">
+            <img src="https://media.pathe.nl/thumb/360x508/gfx_content/posterhr/Kruimeltje_ps_1_jpg_sd-high_Copyright-2019-WW-Entertainment.jpg" alt="movie">
+          </a>
+        </div>
+        <div>
+          <a href="">
+            <img src="https://media.pathe.nl/thumb/360x508/gfx_content/posterhr/Frozen-2-NL-_ps_1_jpg_sd-high_Copyright-2019-Disney-Enterprises-Inc-All-Rights-Reserved.jpg" alt="movie">
+          </a>
+        </div>
+      </div>
     </div>
   </section>
 </main>
