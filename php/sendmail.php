480b51cfcfdbc1bd83f593670956bf5526d21500
Daanisaanwezig <daanisaanwezig@outlook.com>
2020-03-24 10:53:14 +0100

    Mail update

--- php/sendmail.php
+++ php/sendmail.php
@@ -32,12 +32,12 @@ $mail = new PHPMailer(true);
         //Server settings
         $mail->SMTPDebug = SMTP::DEBUG_SERVER;                          // Enable verbose debug output
         $mail->isSMTP();                                                // Send using SMTP
-        $mail->Host       = 'smtp.gmail.com';                           // Set the SMTP server to send through
+        $mail->Host       = 'smtp.ziggo.nl';                           // Set the SMTP server to send through
         $mail->SMTPAuth   = true;                                       // Enable SMTP authentication
-        $mail->Username   = 'daanisaanwezigdev@gmail.com';              // SMTP username
-        $mail->Password   = 'projectBioscoop';                          // SMTP password
+        $mail->Username   = 'curiotest@ziggo.nl';              // SMTP username
+        $mail->Password   = '3XVuL4m:$TDvXn*';                          // SMTP password
         $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;             // Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` also accepted
-        $mail->Port       = 25;                                         // TCP port to connect to
+        $mail->Port       = 587;                                         // TCP port to connect to
 
         //Recipients
         $mail->setFrom('daanisaanwezigdev@gmail.com', 'Bioscoop AMO');
