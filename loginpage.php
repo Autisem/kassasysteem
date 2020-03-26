3fbc8318836a2bf32b9431008bd6f5a551e553ea
Gnol Gnart <longtrang@live.nl>
2020-03-26 10:55:25 +0100

    make login theme same as register & clean code

--- loginpage.php
+++ loginpage.php
@@ -4,16 +4,13 @@
 <main>
   <div class="flexcent">
     <div class="login">
-      <h1>Login</h1>
-      <form action="login_systeem/login.php" method="post">
+      <form class="login-wrapper" action="login_systeem/login.php" method="post">
+        <h1>Login</h1>
         <input type="text" name="email-input" placeholder="e-mail address" required="required" />
         <input type="password" name="password-input" placeholder="Password" required="required" />
         <button type="submit" class="btn btn-primary btn-block btn-large">Let me in.</button>
       </form>
       <p class="account-info">Have you forgotten your password, Don't worry just click <a href="wachtwoord_vergeten.php">here!</a> to find it</p>
-    </div>
-    <div class="register">
-
     </div>
   </div>
 </main>
