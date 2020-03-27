9543473ed798076501eff576a62fb66079946155
Gnol Gnart <longtrang@live.nl>
2020-03-27 11:20:28 +0100

    Make wachtwoord_vergeten.php fit with theme

--- wachtwoord_vergeten.php
+++ wachtwoord_vergeten.php
@@ -4,9 +4,9 @@
 <main>
   <div class="flexcent">
     <div class="wachtwoord">
-      <h1 class="text-center">Wachtwoord vergeten?</h1>
-      <p>Wat jammer dat u u wachtwoord bent veregeten maar dat is geen probleem.<br> Als u hier onder u email adress invult dan sturen wij u een email waar via u u wachtwoord kunt veranderen</p>
-      <form class="" action="login_systeem/wachtwoord_vergeten/wachtwoord_vergeten.php" method="post">
+      <form class="login-wrapper" action="login_systeem/wachtwoord_vergeten/wachtwoord_vergeten.php" method="post">
+        <h1 class="text-center">Wachtwoord vergeten?</h1>
+        <p>Wat jammer dat u u wachtwoord bent veregeten maar dat is geen probleem.<br> Als u hier onder u email adress invult dan sturen wij u een email waar via u u wachtwoord kunt veranderen</p>
         <div class="email-input" id="email-input">
           <label for="email" id="email-input">Email addres:</label>
           <input type="email" name="email" id="email-input">
